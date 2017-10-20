2df538101655dc89f8e830a984ce02664a5fc557
Whit <whitneyrscott@iMac-4.local>
2017-10-19 17:28:26 -0700

    Fix repo issue

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Adam',
+		photo: 'http://lorempixel.com/output/people-q-c-400-200-3.jpg',
+		scores: [
+			'4',
+			'2',
+			'2',
+			'4',
+			'2',
+			'2',
+			'4',
+			'4',
+			'4',
+			'2'
+		]
+	},
+	{
+		name: 'Eve',
+		photo: 'http://lorempixel.com/output/people-q-c-400-200-9.jpg',
+		scores: [
+			'1',
+			'2',
+			'4',
+			'4',
+			'4',
+			'4',
+			'4',
+			'1',
+			'4',
+			'2'
+		]
+	},
+	{
+		name: 'Steve',
+		photo: 'http://lorempixel.com/output/people-q-c-400-200-2.jpg',
+		scores: [
+			'2',
+			'1',
+			'3',
+			'4',
+			'4',
+			'2',
+			'2',
+			'4',
+			'4',
+			'4'
+		]
+	},
+	{
+		name: 'Libby',
+		photo: 'http://lorempixel.com/output/people-q-c-400-200-1.jpg',
+		scores: [
+			'2',
+			'4',
+			'5',
+			'4',
+			'1',
+			'3',
+			'4',
+			'1',
+			'1',
+			'2'
+		]
+	},
+	{
+		name: 'Carl',
+		photo: 'http://lorempixel.com/output/business-q-c-400-200-2.jpg',
+		scores: [
+			'4',
+			'4',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2',
+			'5',
+			'2',
+			'4'
+		]
+	},
+	{
+		name: 'Missy',
+		photo: 'http://lorempixel.com/output/fashion-q-c-400-200-7.jpg',
+		scores: [
+			'4',
+			'3',
+			'2',
+			'4',
+			'2',
+			'5',
+			'3',
+			'4',
+			'4',
+			'2'
+		]
+	},
+	{
+		name: 'Fred',
+		photo: 'http://lorempixel.com/output/business-q-c-400-200-8.jpg',
+		scores: [
+			'3',
+			'1',
+			'2',
+			'5',
+			'2',
+			'2',
+			'5',
+			'5',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Lauren',
+		photo: 'http://lorempixel.com/output/fashion-q-c-400-200-1.jpg',
+		scores: [
+			'2',
+			'5',
+			'2',
+			'4',
+			'3',
+			'4',
+			'1',
+			'4',
+			'4',
+			'2'
+		]
+	},
+	{
+		name: 'Ben',
+		photo: 'http://lorempixel.com/output/business-q-c-400-200-1.jpg',
+		scores: [
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'3',
+			'2',
+			'1'
+		]
+	},
+	{
+		name: 'Leah',
+		photo: 'http://lorempixel.com/output/fashion-q-c-400-200-8.jpg',
+		scores: [
+			'5',
+			'3',
+			'4',
+			'5',
+			'3',
+			'1',
+			'3',
+			'2',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
